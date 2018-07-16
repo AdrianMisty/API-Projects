ec0c93fa657fc2d17cbb5fd2b986174c3a24be89
AdrianMisty <adriana.a.alva@gmail.com>
2018-07-15 22:48:48 -0500

    Update BotChatMain.java

--- BotChatMain.java
+++ BotChatMain.java
@@ -1,13 +1,18 @@
+// Name : AdrianMisty
+// 07-01-2018
 import org.jibble.pircbot.*;
 
 public class BotChatMain {
 	
 	public static void main(String[] args) throws Exception{
-		//connecting to the server
+		//start bot up
 		BotChat bot = new BotChat();
+		//enable debugging output
 		bot.setVerbose(true);
+		//connect to the IRC server
 		bot.connect("irc.freenode.net");
-		bot.joinChannel("#KhansClass");
+		//join the #pircbot channel
+		bot.joinChannel("#pircbot");
 
 	}
 }
